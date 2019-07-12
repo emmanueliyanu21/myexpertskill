98948c09dfd4f3fd8427a088d6ec0a83056700ee
Emmanuel Iyanu <emmanueliyanu21@gmail.com>
2019-07-12 17:16:07 +0100

    few changes

--- server.js
+++ server.js
@@ -49,6 +49,6 @@ app.set('*', (req, res) => {
 // Set Port
 const port = process.env.PORT || 3000;
 
-app.listen(3000, () => {
-    console.log(`listening on port 3000`);
+app.listen(port, () => {
+    console.log(`listening on port ${port}`);
 });
