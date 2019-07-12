0c5bbd3523cf53534da47476e625d828bd645a74
Emmanuel Iyanu <emmanueliyanu21@gmail.com>
2019-07-12 17:46:54 +0100

    one change

--- server.js
+++ server.js
@@ -16,9 +16,6 @@ mongoose.connect(mongoDbUrl, { useNewUrlParser: true }).then(db => {
 
 const { select } = require('./helpers/handlebars-helpers');
 
-// Load routes 
-const index = require('./routes/index');
-
 // set engine
 app.engine('handlebars', exphbs({ defaultLayout: 'home', helpers: { select: select } }));
 app.set('view engine', 'handlebars');
@@ -33,13 +30,12 @@ app.use(passport.session());
 
 //Angular dist output folder
 app.use(express.static(path.join(__dirname, 'dist/myexpertskill')));
-app.use(express.static(path.join(__dirname, 'src/app')));
+
 //Load routes
-const register = require('./routes/index');
+const index = require('./routes/index');
 
 //Use Routes 
 app.use('/index', index);
-app.use('/register', path.join(__dirname, 'src/app/pages/register') );
 
 // send all other request to angular app
 app.set('*', (req, res) => {
